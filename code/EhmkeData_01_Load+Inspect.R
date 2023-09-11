d38d6f0436783bc57eff7c4714b6bd1bcc37b52c
Sam Zipper <samzipper@ku.edu>
2023-09-11 10:20:11 -0500

    Fix VG VWC calculation

--- code/EhmkeData_01_Load+Inspect.R
+++ code/EhmkeData_01_Load+Inspect.R
@@ -16,16 +16,51 @@ thetaR <- 0.034
 thetaS <- 0.46
 alpha <- 1.6 # [m] = 0.016 [1/cm]
 n <- 1.37
+kPa_to_m <- 1/9.804139432 # conversion factor, swp in kPa to m; 9.8 kPa per m
 
-df_raw$vwc_12cm <- VG_ThetaFromHead(df_raw$swp_kPa_12cm*9.804139432, # convert to meters
+df_raw$vwc_12cm <- VG_ThetaFromHead(df_raw$swp_kPa_12cm*kPa_to_m, # convert to meters
                                     ThetaR = thetaR, ThetaS = thetaS, alpha = alpha, n = n)
-df_raw$vwc_47cm <- VG_ThetaFromHead(df_raw$swp_kPa_47cm*9.804139432, # convert to meters
+df_raw$vwc_47cm <- VG_ThetaFromHead(df_raw$swp_kPa_47cm*kPa_to_m, # convert to meters
                                     ThetaR = thetaR, ThetaS = thetaS, alpha = alpha, n = n)
-df_raw$vwc_96cm <- VG_ThetaFromHead(df_raw$swp_kPa_96cm*9.804139432, # convert to meters
+df_raw$vwc_96cm <- VG_ThetaFromHead(df_raw$swp_kPa_96cm*kPa_to_m, # convert to meters
                                     ThetaR = thetaR, ThetaS = thetaS, alpha = alpha, n = n)
-df_raw$vwc_152cm <- VG_ThetaFromHead(df_raw$swp_kPa_152cm*9.804139432, # convert to meters
+df_raw$vwc_152cm <- VG_ThetaFromHead(df_raw$swp_kPa_152cm*kPa_to_m, # convert to meters
                                      ThetaR = thetaR, ThetaS = thetaS, alpha = alpha, n = n)
 
-# inspect
-ggplot(df_raw, aes(x = Datetime, y = swp_kPa_152cm)) +
-  geom_point()
+# inspect - ggplot
+df_raw |> 
+  dplyr::select(Datetime, vwc_12cm, vwc_47cm, vwc_96cm, vwc_152cm) |> 
+  pivot_longer(-Datetime, names_to = "Depth", values_to = "VWC") |> 
+  ggplot(aes(x = Datetime, y = VWC, color = Depth)) + 
+  geom_line()
+
+# inspect - ggplot
+df_raw |> 
+  dplyr::select(Datetime, swp_kPa_12cm, swp_kPa_47cm, swp_kPa_96cm, swp_kPa_152cm) |> 
+  pivot_longer(-Datetime, names_to = "Depth", values_to = "SWP") |> 
+  ggplot(aes(x = Datetime, y = SWP, color = Depth)) + 
+  geom_line() +
+  scale_y_log10()
+
+
+# inspect - dygraph
+library(dygraphs)
+library(xts)
+
+#format data
+df_xts <- df_raw |> 
+  dplyr::select(Datetime, vwc_12cm, vwc_47cm, vwc_96cm, vwc_152cm)
+  
+df_xts<-xts(df_xts, order.by=df_xts$Datetime)
+df_xts<-df_xts[,-1]
+
+#Plot
+dygraph(df_xts) %>%
+  dyRangeSelector() %>%
+  dyLegend() %>%
+  dyOptions(strokeWidth = 1.5) %>%
+  dyOptions(labelsUTC = TRUE) %>%
+  dyHighlight(highlightCircleSize = 5,
+              highlightSeriesBackgroundAlpha = 0.2,
+              hideOnMouseOut = FALSE) %>%
+  dyAxis("y")
